9ece8fcf0586fde903615d7f033dedd23baf8c1b
Andrew Bowler <bowlerandrewj@gmail.com>
2016-03-30 19:46:16 -0500

    Initial commit - Add Unity and Wwise projects

---
+++ Space Shooter/Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private Transform trans;
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+            gameController = gameControllerObject.GetComponent<GameController>();
+        if (gameController == null)
+            Debug.Log("Cannot find 'GameController' script");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+            return;
+        trans = GetComponent<Transform>();
+        Instantiate(explosion, trans.position, trans.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }    
+}
---
+++ Space Shooter/Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Transform trans;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        trans = GetComponent<Transform>();
+        rb.velocity = trans.forward * speed;
+    }
+}
